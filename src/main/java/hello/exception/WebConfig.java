b5a945c1596b882698a804d71ca2a91c1b8d0eb4
byulika2 <byulika2@gmail.com>
2022-04-14 03:01:11 +0900

    서블릿 예외 처리 - 인터셉터

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -4,17 +4,26 @@ import static javax.servlet.DispatcherType.ERROR;
 import static javax.servlet.DispatcherType.REQUEST;
 
 import hello.exception.filter.LogFilter;
+import hello.exception.interceptor.LogInterceptor;
 import java.util.Collections;
 import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-  @Bean
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new LogInterceptor())
+        .order(1)
+        .addPathPatterns("/**")
+        .excludePathPatterns("/css/**", "*.ico", "error"/*, "error-page/**"*/); // 오류 페이지 경로
+  }
+
+  //  @Bean
   public FilterRegistrationBean lgoFilter() {
     FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
     filterRegistrationBean.setFilter(new LogFilter());
