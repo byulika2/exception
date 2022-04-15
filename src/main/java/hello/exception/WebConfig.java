55d9b504f33086344cd78dd078b7b7ee8ec894cf
byulika2 <byulika2@gmail.com>
2022-04-16 02:50:10 +0900

    HandlerExceptionResolver 활용

--- src/main/java/hello/exception/WebConfig.java
+++ src/main/java/hello/exception/WebConfig.java
@@ -6,6 +6,7 @@ import static javax.servlet.DispatcherType.REQUEST;
 import hello.exception.filter.LogFilter;
 import hello.exception.interceptor.LogInterceptor;
 import hello.exception.resolver.MyHandleExceptionResolver;
+import hello.exception.resolver.UserHandleExceptionResolver;
 import java.util.Collections;
 import java.util.List;
 import javax.servlet.Filter;
@@ -29,6 +30,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
     resolvers.add(new MyHandleExceptionResolver());
+    resolvers.add(new UserHandleExceptionResolver());
   }
 
   //  @Bean
